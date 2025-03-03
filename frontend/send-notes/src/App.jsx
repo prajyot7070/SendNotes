29b815e6a96e51148cd02bd4527b5a7c33d03a2e
redekarlk <redekarlk123@gmail.com>
2025-03-03 10:05:47 +0530

    froented created

---
+++ frontend/send-notes/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text font-bold text">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+
+export default App
